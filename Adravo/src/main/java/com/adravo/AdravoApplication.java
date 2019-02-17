fd508d0bb1e5cb264d5e35946f3774194948c703
ahalamus <adam.halamus@gmail.com>
2019-02-17 11:02:50 +0100

    Init project

---
+++ Adravo/src/main/java/com/adravo/AdravoApplication.java
@@ -0,0 +1,13 @@
+package com.adravo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdravoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdravoApplication.class, args);
+    }
+
+}
