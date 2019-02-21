1ba58ad0eb4fa3c47b06317f5f4ebe44ddb7e7a7
ahalamus <adam.halamus@gmail.com>
2019-02-21 21:07:01 +0100

    Add project base structure

--- Adravo/application/src/main/java/com/adravo/AdravoMain.java
+++ Adravo/application/src/main/java/com/adravo/AdravoMain.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdravoApplication {
+public class AdravoMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdravoApplication.class, args);
+        SpringApplication.run(AdravoMain.class, args);
     }
-
 }
